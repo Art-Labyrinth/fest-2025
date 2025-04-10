3df5cbd4a6538767d741a5797f125d04a5ef4bcf
Gleep4711 <gleep4711@gmail.com>
2025-04-10 23:28:51 +0300

    Add ABOUT component with information about the festival and update routing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import MainPage from "./components/Main/MainPage";
 // import Soon from "./components/Coming/Soon";
+import About from "./components/About";
+import { Header } from "./components/Header/Header";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        {/* <Route path="/" element={<Soon />} /> */}
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <main className="min-h-screen h-full min-w-screen w-full">
+              <Header />
+              <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/about" element={<About />} />
+                    {/* <Route path="/" element={<Soon />} /> */}
+                </Routes>
+            </main>
+        </BrowserRouter>
+    </React.StrictMode>
 );
