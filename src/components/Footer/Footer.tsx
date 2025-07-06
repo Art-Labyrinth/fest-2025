e5d1c66feea8e28f56e1e3272185c24f74487c1e
Marina Mikitenko <oce4ka@gmail.com>
2025-07-07 01:12:16 +0200

    add sponsors (#72)
    * content changes on page getting there
    
    * content changes on page getting there
    
    * add schedule page
    
    * fixes in dates and prices
    
    * removed Matilda and Teiu from Program
    
    * add schedule
    
    * add schedule EN and RO
    
    * add sponsors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({backgroundColor = "bg-main"}) {
 
   return (
     <footer className={`${backgroundColor}`}>
-      <div className="px-4 max-w-[1452px] py-4 mx-auto flex justify-between align-middle flex-wrap md:flex-nowrap gap-[20px] md:gap-0">
+      <div className="px-4 max-w-[1452px] py-4 mx-auto flex justify-between align-middle flex-wrap xl:flex-nowrap gap-[20px] md:gap-0">
         <div className="flex w-[100%]">
           <div className="w-[35px] sm:w-[70px] me-[23px] sm:me-[60px] flex-shrink-0 self-center" onClick={() => navigate('/')}>
             <img src="https://files.art-labyrinth.org/logo.svg" alt=""/>
@@ -50,13 +50,15 @@ export default function Footer({backgroundColor = "bg-main"}) {
             </div>
           </div>
         </div>
-        <div className="flex flex-wrap w-[100%] md:w-auto md:flex-nowrap ms-0 md:ms-[12px] gap-[12px] sm:gap-[14px] justify-start md:justify-items-end justify-self-end flex-shrink-0 md:justify-end align-middle">
-          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://numina.md/"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[140px] lg:h-[140px]" src="/Footer/numina.jpg" alt="Numina"/></a>
-          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://www.instagram.com/shanti.space.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[140px] lg:h-[140px]" src="/Footer/shanti.png" alt="Shanti"/></a>
+        <div className="flex flex-wrap w-[100%] md:w-auto ms-0 md:ms-[12px] mt-[30px] xl:mt-0 gap-[12px] sm:gap-[14px] justify-start md:justify-items-end justify-self-end flex-shrink-0 md:justify-end align-middle">
+          <a target="_blank" rel="noopener noreferrer" className="bg-[#fff] flex-shrink-0 self-center" href="https://www.danishculture.com/eu4culture/"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/dansk-kultur-institut.png" alt="Dansk-kultur-institut"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://www.instagram.com/artavivusmd/"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/artavivus.jpg" alt="artavivus"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://numina.md/"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/numina.jpg" alt="Numina"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://www.instagram.com/shanti.space.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/shanti.png" alt="Shanti"/></a>
 
-          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://growhills.com"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[140px] lg:h-[140px]" src="/Footer/growhills.png" alt="Growhills"/></a>
-          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://pergament.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[140px] lg:h-[140px]" src="/Footer/pergament.png" alt="Pergament"/></a>
-          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://xstyle.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[140px] lg:h-[140px]" src="/Footer/x-style.png" alt="X-Style"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://growhills.com"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/growhills.png" alt="Growhills"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://pergament.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/pergament.png" alt="Pergament"/></a>
+          <a target="_blank" rel="noopener noreferrer" className="flex-shrink-0 self-center" href="https://xstyle.md"><img className="w-[80px] h-[80px] md:w-[85px] md:h-[85px] lg:w-[130px] lg:h-[130px]" src="/Footer/x-style.png" alt="X-Style"/></a>
         </div>
       </div>
     </footer>
